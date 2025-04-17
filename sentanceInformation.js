b64c53ecdf0f274ec473f3369654379d8e7cdbaa
Edward <97116266+Anzkiii@users.noreply.github.com>
2025-04-17 16:22:05 +0300

    main

---
+++ sentanceInformation.js
@@ -0,0 +1,30 @@
+function getSentanceInformation(...sentances){
+    let wordCount = 0;
+    let letterCount = 0;
+    let sentanceCount = 0;
+    
+    for (let i in sentances){
+
+        for (let letter of sentances[i]){
+            if (letter !== " "){
+                letterCount++;
+            }
+        }
+        wordCount += sentances[i].split(" ").length
+        sentanceCount++;
+    };
+    return {
+        wordCount: wordCount,
+        letterCount: letterCount,
+        sentanceCount: sentanceCount
+    }
+}
+
+
+
+sentance = getSentanceInformation("Hello world", "ababa", "This is good");
+
+
+console.log(sentance.wordCount);
+
+console.log(sentance.letterCount);
