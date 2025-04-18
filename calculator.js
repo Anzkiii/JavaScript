625577e60f22cfa990f2b2a00bc3a33c576377d0
Edward <97116266+Anzkiii@users.noreply.github.com>
2025-04-18 14:18:57 +0300

    main

--- calculator.js
+++ calculator.js
@@ -26,3 +26,5 @@ console.log(calculator("12 - 15"))
 
 
 
+
+
---
+++ continuousArray.js
@@ -0,0 +1,15 @@
+
+
+
+var continuousArray = [1,2,3,4,5]
+
+while (continuousArray[continuousArray.length - 1] != 1000){
+
+    console.log(continuousArray);
+    continuousArray.push(continuousArray[continuousArray.length - 1] + 1)
+    continuousArray.shift()
+
+};
+
+
+
